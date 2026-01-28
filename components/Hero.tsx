1b23390997ca299f35d83c1393f084b6368e625c
ABHISHEK VISHNOI <139157548+abhishekxdev@users.noreply.github.com>
2026-01-28 10:34:00 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
             </span>
-            Slots Available for December
+            Slots Available for January
           </p>
         </div>
 
