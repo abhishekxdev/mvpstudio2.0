763d3cbaa1d77a5e3a9af3f1d39c47f9623d4018
ABHISHEK VISHNOI <139157548+abhishekxdev@users.noreply.github.com>
2026-04-04 15:50:42 +0530

    Updated build-manifest.json

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/chotelal/Downloads/project 2/app/layout.tsx
+// File: /home/project/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/chotelal/Downloads/project 2/app/page.tsx
+// File: /home/project/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
